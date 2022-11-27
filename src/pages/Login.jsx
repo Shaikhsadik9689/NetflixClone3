c047a32390ef3ebdfaccb8a13d8b57b26fd2cc2d
sadikshaik@patent-art <sadikshaik@patent-art>
2022-11-27 15:19:28 +0530

    Changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,104 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import BackgroundImage from "../components/Backgroundmage";
+import Header from "../components/Header";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
 
-const Login = () => {
-    return (
-        <Container>
-        <div className='wel'>
-            <h1>Welcome to log in page pe kam karo</h1>
-            <h2>ho gaya</h2>
-            <h3>dalo kuch to bhi</h3>
+function Login() {
+    const navigate = useNavigate();
+    const [formValues, setFormValues] = useState({
+        email: "",
+        password: "",
+    });
+
+  const handleLogin = async () => {
+    try {
+      const {email, password} = formValues;
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
+  return (
+    <Container>
+      <BackgroundImage />
+      <div className="content">
+        <Header />
+        <div className="form-container flex column a-center j-center">
+          <div className="form flex column a-center j-center">
+            <div className="title">
+              <h3>Login</h3>
+            </div>
+            <div className="container flex column">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                onChange={(e) => setFormValues({...formValues,[e.target.name]:e.target.value,})}
+                value={formValues.email}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => setFormValues({...formValues,[e.target.name]:e.target.value,})}
+                value={formValues.password}
+              />
+              <button onClick={handleLogin}>Log in</button>
+            </div>
+          </div>
         </div>
-        </Container>
-    );
-};
-const Container = styled.div`
-.wel{
-    text-Align: center;
-    margin-Top: 300px;
-    font-size: 50px;
+      </div>
+    </Container>
+  );
 }
-.wel:hover{
-    box-shadow: inset 1500px 0 0 0 #54b3d6;
-    color: white;
-    transform: scaleX(0.5) scaleY(1);
 
-`
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.5);
+    grid-template-rows: 15vh 85vh;
+    .form-container {
+      gap: 2rem;
+      height: 85vh;
+      .form {
+        padding: 2rem;
+        background-color: #000000b0;
+        width: 25vw;
+        gap: 2rem;
+        color: white;
+        .container {
+          gap: 2rem;
+          input {
+            padding: 0.5rem 1rem;
+            width: 15rem;
+          }
+          button {
+            padding: 0.5rem 1rem;
+            background-color: #e50914;
+            border: none;
+            cursor: pointer;
+            color: white;
+            border-radius: 0.2rem;
+            font-weight: bolder;
+            font-size: 1.05rem;
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigation } from 'react-router-dom';
 import styled from "styled-components";
 import Backgroundimage from '../components/Backgroundmage';
 import Header from '../components/Header';
+import {firebaseAuth} from "../utils/firebase-config";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 const Signup = () => {
+    const navigate = useNavigation();
+    const [showPassword, setShowPassword] = useState(false);
+    const [formValues, setFormValues] = useState({
+        email: "",
+        password: "",
+    });
+     const handleSignIn = async () => {
+        try{
+            const {email, password} = formValues;
+            await createUserWithEmailAndPassword(firebaseAuth, email, password);
+        }catch(err) {
+            console.log(err);
+        }
+     };
+     onAuthStateChanged(firebaseAuth, (currentUser) => {
+        if(currentUser) navigate("/");
+     });
     return (
         <div>
-            <Container>
+            <Container showPassword = {showPassword}>
                 <Backgroundimage/>
                 <div className='content'>
                 <Header login/>
@@ -18,12 +38,17 @@ const Signup = () => {
                             Ready to watch? Enter Your email to create or restart membership
                         </h6>
                     </div>
-                    <div className='from'>
-                        <input type="email" placeholder='Email address' name='email' />
-                        <input type="password" placeholder='Password' name='password' />
-                        <button>Get Start</button>
+                    <div className='form'>
+                        <input type="email" placeholder='Email address' name='email' value={formValues.email} onChange={(e) => setFormValues({...formValues,[e.target.name]:e.target.value})} />
+                        {
+                            showPassword && (<input type="password" placeholder='Password' name='password' value={formValues.password} onChange={(e) => setFormValues({...formValues,[e.target.name]:e.target.value})} />)
+                        }
+                        
+                        {
+                            ! showPassword && (<button onClick={() => setShowPassword(true)}>Get Start</button>)
+                        }
                     </div>
-                    <button>Log In</button>
+                    <button onClick={handleSignIn}>Log In</button>
                 </div>
                 </div>
             </Container>
@@ -42,6 +67,51 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows:15vh 85vh;
+    .body {
+        gap:1rem;
+        .text {
+            gap: 1rem;
+            text-align: center;
+            font-size: 2rem;
+            h1 {
+                padding: 0.25rem;
+            }
+        }
+        .form {
+            display: grid;
+            grid-template-column:${({showPassword}) => showPassword ? "1fr 1fr" : "2fr 1fr"}
+            width: 60%;
+            input{
+                color: black;
+                border: none;
+                padding: 1.5rem;
+                font-size: 1.2rem;
+                border: 1px solid black;
+                &:focus {
+                    outline: none;
+                }
+            }
+            button{
+                padding: 0.5rem 1rem;
+                background-color: red;
+                border: none;
+                cursor: pointer;
+                color: white;
+                font-weight: border;
+                font-size: 1.05rem;
+            }
+        }
+        button{
+            padding: 0.5rem 1rem;
+            background-color: red;
+            border: none;
+            cursor: pointer;
+            color: white;
+            border-radius: 0.2rem;
+            font-weight: border;
+            font-size: 1.05rem;
+        }
+    }
   }
 `;
 export default Signup;
